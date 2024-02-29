a23af9471cbce2bfe09b9b6ea37cb847208858db
madebyparry <nicholas@madebyparry.com>
2024-02-29 18:39:36 -0500

    list functionality basic styles x3

--- check_off_items.php
+++ check_off_items.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $list_handler = fopen($cwd . '/' . $list_file, 'w');
     // Write to list
     foreach ($remaining as $key => $value) {
-        fwrite($list_handler, $value);
+        fwrite($list_handler, str_replace("_", " ", $value));
     }
     // Close handler
     fclose($list_handler);
