29becba7aa6cb99e20aed1854b74603ae0768095
madebyparry <nicholas@madebyparry.com>
2024-02-29 20:01:24 -0500

    list functionality first wave styles x5

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $devel_enable = TRUE;
 </head>
 <body>
     <?php 
-    require_once('./header.');
+    require_once('./header.php');
     session_start();
     if (isset($_SESSION['status_message'])): ?>
     <div class="status-message">
