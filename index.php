7a47b312d28a77cac6b2ddcfd81ad64dc3fbc343
madebyparry <nicholas@madebyparry.com>
2024-02-29 22:04:17 -0500

    list functionality first wave styles x20

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include 'constants.php';
 $devel_enable = TRUE;
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height: 100%;">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
