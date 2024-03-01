98fd914a5e5cb7b03fa423a7dc02182be68bb1bf
madebyparry <nicholas@madebyparry.com>
2024-02-29 20:10:21 -0500

    list functionality first wave styles x6

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php 
+    include 'constants.php';
+?>
+
+<div class="site-header">
+  <h1><a href="./index.php">Simple List</a></h1>
+  <nav>
+    <ul>
+      <li><a href="https://madebyparry.com/">MBP</a></li>
+      <li><a href="./about.php">About</a></li>
+      <li><a href="./problem.php">Flag a problem</a></li>
+      <li><a href="https://github.com/madebyparry/simple_list/">Github</a></li>
+    </ul>
+  </nav>
+</div>
