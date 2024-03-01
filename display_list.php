5cafe0b02e60c4fe9d5a2214954abf268799d9b5
madebyparry <nicholas@madebyparry.com>
2024-03-01 13:47:21 -0500

    list functionality first wave styles x24

--- display_list.php
+++ display_list.php
@@ -6,11 +6,12 @@
     $list_handler = fopen($cwd . '/' . $list_file, 'r');
     while (($line = fgets($list_handler)) !== false) {
         $item_key = trim(str_replace(' ', '_', str_replace('\n', '', $line)));
+        echo '<div class="list-item">';
         echo '<input type="checkbox" name="'.$item_key.'" value="checked" class="list_item_checkbox" id="checkbox_'.$item_key.'" onclick="check_item(this)"/>';
         echo '<label class="'.$item_key.'" for="checkbox_'.$item_key.'">';
         echo $line;
         echo '</label>';
-        echo '<hr/>';
+        echo '</div>';
     }
     fclose($list_handler);
     
